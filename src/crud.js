148be525e5e85af8e436772cdfc431766eb8db77
Kehinde Onifade <onifkay@gmail.com>
2022-08-10 19:29:11 +0100

    Create ToDo class

---
+++ src/crud.js
@@ -0,0 +1,11 @@
+import dots from '../assets/images/dots.png';
+import deleteIcon from '../assets/images/delete.png';
+import saveIcon from '../assets/images/save.jpeg';
+
+class ToDo {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
